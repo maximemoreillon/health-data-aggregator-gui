1168683fefe619b4cecb0eb4eae3896eb6368520
Maxime Moreillon <m.moreillon@gmail.com>
2022-09-17 17:22:18 +0900

    init

--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,15 @@ const routes = [
     component: HomeView
   },
   {
+    path: '/bloodsugar',
+    name: 'bloodSugar',
+    component: () => import('../views/BloodSugar.vue')
+  },
+  {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+
+    component: () => import('../views/AboutView.vue')
   }
 ]
 
---
+++ src/views/BloodSugar.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card class="mx-auto" max-width="40rem">
+    <v-card-title>
+      Blood sugar level
+    </v-card-title>
+    <v-card-text>
+      <v-form @submit.prevent="submit()">
+        
+      </v-form>
+      <v-row align="baseline">
+        <v-col>
+          <v-text-field v-model.number="bloodSugarlevel" label="Blood sugar level" suffix="UNIT" />
+        </v-col>
+        <v-col cols="auto">
+          <v-btn>Submit</v-btn>
+        </v-col>
+      </v-row>
+      
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+
+export default {
+  name: 'BloodSugar',
+  data(){
+    return {
+      bloodSugarlevel: 0
+    }
+  }
+}
+</script>
