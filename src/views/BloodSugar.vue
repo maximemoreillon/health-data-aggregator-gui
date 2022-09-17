af305afe509ed498ecd054d7a3b5e7e0fd37f6d9
Maxime Moreillon <m.moreillon@gmail.com>
2022-09-17 17:36:31 +0900

    blood sugar level registration

--- src/views/BloodSugar.vue
+++ src/views/BloodSugar.vue
@@ -5,16 +5,16 @@
     </v-card-title>
     <v-card-text>
       <v-form @submit.prevent="submit()">
-        
+        <v-row align="baseline">
+          <v-col>
+            <v-text-field v-model.number="bloodSugarLevel" label="Blood sugar level" suffix="UNIT" />
+          </v-col>
+          <v-col cols="auto">
+            <v-btn type="submit" :loading="loading">Submit</v-btn>
+          </v-col>
+        </v-row>
       </v-form>
-      <v-row align="baseline">
-        <v-col>
-          <v-text-field v-model.number="bloodSugarlevel" label="Blood sugar level" suffix="UNIT" />
-        </v-col>
-        <v-col cols="auto">
-          <v-btn>Submit</v-btn>
-        </v-col>
-      </v-row>
+      
       
     </v-card-text>
   </v-card>
@@ -26,7 +26,24 @@ export default {
   name: 'BloodSugar',
   data(){
     return {
-      bloodSugarlevel: 0
+      bloodSugarLevel: 0,
+      loading: false,
+    }
+  },
+  methods: {
+    async submit(){
+      this.loading = true
+      try {
+        const route = '/measurements/bloodsugar'
+        const body = { bloodSugarLevel: this.bloodSugarLevel }
+        await this.axios.post(route, body)
+      } catch (error) {
+        console.error(error)
+      }
+      finally {
+        this.loading = false
+      }
+      
     }
   }
 }
