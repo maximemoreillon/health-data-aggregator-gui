7ccde9598f9f11364a1099f8c67bfa2b2e2b6f49
Maxime Moreillon <m.moreillon@gmail.com>
2022-09-18 17:13:15 +0900

    home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,21 @@
 <template>
-  <v-card>
+  <v-card max-width="50rem" class="mx-auto">
     <v-card-title>
       Health data aggregator
     </v-card-title>
+    <v-card-text>
+      <v-list dense nav>
+        <v-list-item v-for="(item, index) in nav" :key="`nav_item_${index}`" :to="item.to" exact>
+          <v-list-item-icon>
+            <v-icon>{{item.icon}}</v-icon>
+          </v-list-item-icon>
+      
+          <v-list-item-content>
+            <v-list-item-title>{{item.title}}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-card-text>
   </v-card>
 </template>
 
@@ -12,5 +25,15 @@
     name: 'HomeView',
     components: {
     },
+    data(){
+      return {
+        nav: [
+          { title: 'Energy', to: { name: 'energy' }, icon: 'mdi-weight-lifter' },
+          { title: 'Blood sugar levels', to: { name: 'bloodSugar' }, icon: 'mdi-diabetes' },
+          { title: 'Blood pressure', to: { name: 'bloodPressure' }, icon: 'mdi-water' },
+          { title: 'About', to: { name: 'about' }, icon: 'mdi-information-outline' },
+        ]
+      }
+    }
   }
 </script>
